4ae7703b44372a8f66beefb0beb25f67f814f1c5
Jaime Mejia-Beher <jmejiabeher@gmail.com>
2024-05-25 13:52:16 -0600

    Adjusted Envelope css to position absolute the <span>, moved the .envelope input from Envelope.css to Index.css

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,17 @@ import Form from "./components/Form";
 import ExchangeRates from "./components/ExchangeRates";
 import AppContextProvider from "./AppContextProvider";
 import EnvelopesContainer from "./components/EnvelopesContainer";
-// import Header from "./components/Header";
 
 function App() {
   return (
     <AppContextProvider>
-      <div>
-        {/* <Header /> */}
-        <div className="flex-container">
-          <Form />
-          <EnvelopesContainer />
-        </div>
+      {/* <div> */}
+      {/* <Header /> */}
+      <Form />
+      <div className="flex-container">
+        <EnvelopesContainer />
       </div>
+      {/* </div> */}
       <div>
         <ExchangeRates />
       </div>
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,5 @@
 // eslint-disable react/prop-types
 import { useContext } from "react";
-import "./Form.css";
 import { AppContext } from "../AppContextProvider";
 
 const Form = () => {
